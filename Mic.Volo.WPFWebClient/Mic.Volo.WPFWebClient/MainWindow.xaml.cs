8f3d4dc86756cd521ccad22e3c9a429fe8eac854
User <artyom.hovakimyan01@gmail.com>
2019-04-14 16:22:17 +0400

    add  interface frontend part

--- Mic.Volo.WPFWebClient/Mic.Volo.WPFWebClient/MainWindow.xaml.cs
+++ Mic.Volo.WPFWebClient/Mic.Volo.WPFWebClient/MainWindow.xaml.cs
@@ -24,5 +24,61 @@ namespace Mic.Volo.WPFWebClient
         {
             InitializeComponent();
         }
+
+        private void MyaddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ListView conteiner = new ListView()
+            {
+                Name="myConteiner",
+            };
+            TextBox textBox = new TextBox()
+            {
+                Width=600,
+                Height=20,
+                BorderBrush=Brushes.Red
+            };
+           // myListBox.Items.Add(textBox);
+            conteiner.Items.Add(textBox);
+            ProgressBar progressBar = new ProgressBar()
+            {
+                Value=0,
+                Width=600,
+                Height=20
+            };
+            Canvas twpbothbutton = new Canvas()
+            {
+                Name="mycanv",
+                Width=599,
+                Height=30,
+            };
+            // myListBox.Items.Add(progressBar);
+            conteiner.Items.Add(progressBar);
+            Button button = new Button()
+            {
+                Content = "Download",
+                Width=100,
+                Height=30
+            };
+            //myListBox.Items.Add(button);
+            //conteiner.Items.Add(button);
+            Button cancel = new Button()
+            {
+                Content = "Cancel",
+                Width = 100,
+                Height = 30,
+                Margin = new Thickness(120,0,0,10)
+            };
+            
+            twpbothbutton.Children.Add(button);
+            twpbothbutton.Children.Add(cancel);
+            
+            conteiner.Items.Add(twpbothbutton);
+            //conteiner.Items.Add(cancel);
+            myListBox.Items.Add(conteiner);
+            
+
+            
+
+        }
     }
 }
